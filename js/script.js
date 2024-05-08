7d0b46cb8d69baeb8968ba7b2356527c2cdccfa9
Wuyung Pratama <tamayoenk12@gmail.com>
2024-05-08 22:39:12 +0700

    initial config

--- js/script.js
+++ js/script.js
@@ -7,23 +7,36 @@ function validateForm() {
   if (name == "" || email == "" || select == "") {
     alert("Tidak boleh ada yang kosong");
     return false;
+  } else {
+    alert("Data sudah kami terima " + name);
   }
 }
 
 // slideshow automatic
-let index = 0;
-displayImages();
+var slideIndex = 1;
+slideShow(1);
 
-function displayImages() {
-  let i;
-  let images = document.getElementsByClassName("mySlide");
+function plusSlide(n) {
+  slideShow((slideIndex += n));
+}
+
+function slideShow(n) {
+  var i;
+  var images = document.getElementsByClassName("mySlide");
+  if (n > images.length) slideIndex = 1;
+  // else if (n < 1) slideIndex = images.length;
+
   for (i = 0; i < images.length; i++) {
     images[i].style.display = "none";
   }
-  index++;
-  if (index > images.length) {
-    index = 1;
-  }
-  images[index - 1].style.display = "block";
-  setTimeout(displayImages(), 2000);
+
+  // var i = 0;
+  // while (i < images.length) {
+  //   images[i].style.display = "none";
+  //   i++;
+  // }
+
+  images[slideIndex - 1].style.display = "block";
 }
+
+setInterval(() => plusSlide(1), 2000);
